a5c63852fd9718a9dc0dce8b9f894c08f597af6a
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-04-17 16:55:10 -0300

    Added teacher profile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css'
 
 import {Card} from '../../components/Card';
@@ -6,6 +6,7 @@ import {Card} from '../../components/Card';
 export function Home() {
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: '', avatar:''});
 
   function handleAddStudent() {
     const newStudent = {
@@ -19,10 +20,27 @@ export function Home() {
   setStudents(prevState => [...prevState, newStudent]);
   }
 
+useEffect(() => {
+  fetch('https://api.github.com/users/vargastm')
+  .then(response => response.json())
+  .then(data => {
+    setUser({
+      name: data.name,
+      avatar: data.avatar_url,
+    })
+  });
+},[]);
 
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+
       <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
       <button type="button" onClick={handleAddStudent}>Adicionar</button>
       {
